835b7d1718cda58d907447cea174532e23574e65
Matt Fannin <mfannin@umich.edu>
2025-03-07 22:22:14 -0500

    Next steps

--- data_pull_test.py
+++ data_pull_test.py
@@ -7,4 +7,7 @@ print(data) ## Prints a ticker object
 print(data.info)
 print("--------------------")
 print("--------------------")
-print(data.history(period='1y')) ## must be of the format 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max, etc.
+print(data.history(period='max')) ## must be of the format 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max, etc.
+
+nke_df = pd.DataFrame(data.history(period='max'))
+nke_df.head()
