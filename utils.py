746dace1c76662bf2d6203db81df8489b0339b61
Matt Fannin <mfannin@umich.edu>
2025-04-01 19:58:07 -0400

    Commit

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import yfinance as yf
 import ta
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def pull_data(ticker):
     data = yf.Ticker(ticker)
@@ -20,3 +22,18 @@ def pull_data(ticker):
         print("Less than 3 years of data: ", df_filtered.index.min())
 
     return df_filtered
+
+
+def plot_data_line_chart(df, start_date, end_date):
+    # Filter the DataFrame based on the selected date range
+    # filtered_df = df[(df['Date'] >= pd.to_datetime(start_date)) & (df['Date'] <= pd.to_datetime(end_date))]
+    filtered_df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+
+    # Plot using the 'Date' column instead of index
+    plt.figure(figsize=(10, 5))
+    plt.plot(filtered_df['Date'], filtered_df['Close'], label="Close Price")
+    plt.xlabel("Date")
+    plt.ylabel("Stock Price")
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.show()
